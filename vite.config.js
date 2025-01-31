55286f6d639a56578b02ca260be19f14f108f90f
DharshanGitRepo <dharshanssunity@gmail.com>
2025-01-31 10:27:17 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://mern-stack2-plum.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        
+      }
+    }
+  }
 })
